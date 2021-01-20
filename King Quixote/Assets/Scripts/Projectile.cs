b084c9b8a48ecc7477d523af92581d573c3ba18b
Ben <b.albers@digipen.edu>
2021-01-20 14:13:01 -0800

    Crossbow is functional in animtest

---
+++ King Quixote/Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 0.05f;
+    public float lifetime = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x + speed, gameObject.transform.position.y, gameObject.transform.position.z);
+
+        if (lifetime > 0)
+        {
+            lifetime -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ King Quixote/Assets/Scripts/SpawnArrow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArrow : MonoBehaviour
+{
+    public GameObject Projectile;
+    private bool shot = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.GetComponent<AnimationCycle>())
+        {
+            if (gameObject.GetComponent<AnimationCycle>().currentFrame == 2)
+            {
+                if (shot == false)
+                {
+                    Instantiate(Projectile);
+                    shot = true;
+                }
+            }
+
+            if (gameObject.GetComponent<AnimationCycle>().currentFrame == 1)
+            {
+                shot = false;
+            }
+        }
+    }
+}
