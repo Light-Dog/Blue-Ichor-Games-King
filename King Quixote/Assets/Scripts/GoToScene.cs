1c848ebae31a76d5f5137738aafa465666f98853
Ben <b.albers@digipen.edu>
2021-01-23 13:58:21 -0800

    Menus to start and quit
    Level 1 also added

---
+++ King Quixote/Assets/Scripts/GoToScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GoToScene : MonoBehaviour
+{
+
+    public int TargetSceneIndex = 1;
+    public Scene TargetScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void SwitchScene()
+    {
+
+        SceneManager.LoadScene(TargetSceneIndex);
+        //SceneManager.LoadScene(TargetScene.buildIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
