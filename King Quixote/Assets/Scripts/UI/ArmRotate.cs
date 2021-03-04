9bb57a3062712231d787f45b1839d5d7aa1b885f
Weston Baker <wbaker_98@live.com>
2021-03-04 15:08:59 -0800

    ad

--- King Quixote/Assets/Scripts/UI/ArmRotate.cs
+++ King Quixote/Assets/Scripts/UI/ArmRotate.cs
@@ -6,53 +6,57 @@ public class ArmRotate : MonoBehaviour
 {
     public GameObject Pos1Obj;
     public GameObject Pos2Obj;
+    public GameObject Pos3Obj;
     private Transform Pos1;
     private Transform Pos2;
+    private Transform Pos3;
+
     public int currentPos = 1;
     public float swingSpeed = 0.1f;
-    public float screenCenter = 256f;
+    bool move = false;
 
     // Start is called before the first frame update
     void Start()
     {
         Pos1 = Pos1Obj.transform;
         Pos2 = Pos2Obj.transform;
+        Pos3 = Pos3Obj.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.mousePosition.y > screenCenter)
+        if(move)
         {
-            currentPos = 1;
-            //Debug.Log("A "+Input.mousePosition.y);
-        }
-        else
-        {
-            currentPos = 2;
-            //Debug.Log("B "+Input.mousePosition.y);
+            if (currentPos == 1)
+            {
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, Pos1.localRotation, swingSpeed);
+                if (transform.localRotation == Pos1.localRotation)
+                    move = false;
+            }
+            else if (currentPos == 2)
+            {
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, Pos2.localRotation, swingSpeed);
+                if (transform.localRotation == Pos2.localRotation)
+                    move = false;
+            }
+            else if (currentPos == 3)
+            {
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, Pos3.localRotation, swingSpeed);
+                if (transform.localRotation == Pos2.localRotation)
+                    move = false;
+            }
         }
 
-        if (currentPos == 1)
-        {
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, Pos1.localRotation, swingSpeed);
-        }
-        else
-        {
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, Pos2.localRotation, swingSpeed);
-        }
+        
 
     }
-    /*
-    void SetAngle()
+
+    public void MouseToButton(int armPos)
     {
-        if (currentPos == 1)
-        {
-            currentPos += 1;
-        }
-        else
-        {
-            currentPos = 1;
-        }
-    }*/
+        //set new arm and tell it to move
+        move = true;
+        currentPos = armPos;
+    }
+    
 }
